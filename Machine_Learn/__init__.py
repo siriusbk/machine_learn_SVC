31afcedcb1de79194eba709f0563b1eac60b0101
siriusbk <sirius.bakin@gmail.com>
2016-05-28 13:10:07 +0300

    visualize

--- Machine_Learn/__init__.py
+++ Machine_Learn/__init__.py
@@ -12,7 +12,7 @@ plt.show()
 X = nu.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
 y = [0,1,0,1,0,1]
 
-clf = svm.SVC(kernel='liner', C = 1.0)
+clf = svm.SVC(kernel='linear', C = 1.0)
 clf.fit(X,y)
 
 print(clf.predict([0.58,0.76]))
