4a22fc1a70baa79ada3a95b9f91de49e3df82a1a
siriusbk <sirius.bakin@gmail.com>
2016-05-28 12:59:49 +0300

    visualize

--- Machine_Learn/__init__.py
+++ Machine_Learn/__init__.py
@@ -9,4 +9,25 @@ y = [2, 8, 1.8, 8, 0.6, 11]
 plt.scatter(x,y)
 plt.show()
 
-x = np.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
+X = nu.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
+y = [0,1,0,1,0,1]
+
+clf = svm.SVC(kernel='liner', C = 1.0)
+clf.fit(X,y)
+
+print(clf.predict([0.58,0.76]))
+print(clf.predict([10.58,10.76]))
+
+w = clf.coef_[0]
+print(w)
+
+a = -w[0] / w[1]
+
+xx = nu.linspace(0,12)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+plt.scatter(X[:,0], X[:,1], c = y)
+plt.legend()
+plt.show()
