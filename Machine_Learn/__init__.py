c0e21585c73b2d74922d9b3c2c9a27dfd1942ab9
siriusbk <sirius.bakin@gmail.com>
2016-05-28 12:00:31 +0300

    corrections for importing style use

--- Machine_Learn/__init__.py
+++ Machine_Learn/__init__.py
@@ -1,10 +1,2 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from plt import style
-style.use("ggplot")
-
-x = [1, 5, 1.5, 8, 1, 9]
-y = [2, 8, 1.8, 8, 0.6, 11]
-
-plt.scatter(x,y)
-plt.show()
-
