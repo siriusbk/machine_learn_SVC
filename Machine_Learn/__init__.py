75833af5e2cf5a5a15a9622aaa1eac0a4c5a2cc2
siriusbk <sirius.bakin@gmail.com>
2016-05-28 11:46:33 +0300

    corrections for importing style use

--- Machine_Learn/__init__.py
+++ Machine_Learn/__init__.py
@@ -1,10 +1,6 @@
-
-
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
+from plt import style
 style.use("ggplot")
-from sklearn import svm
 
 x = [1, 5, 1.5, 8, 1, 9]
 y = [2, 8, 1.8, 8, 0.6, 11]
