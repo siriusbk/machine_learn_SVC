d77b584083eef2f0a8eee7664903a43319a06a79
siriusbk <sirius.bakin@gmail.com>
2016-05-28 12:46:50 +0300

    from 151 matplotlib version style is not used prevent from using

--- Machine_Learn/__init__.py
+++ Machine_Learn/__init__.py
@@ -1,2 +1,12 @@
-import numpy as np
+import numpy as nu
 import matplotlib.pyplot as plt
+from sklearn import svm
+
+
+x = [1, 5, 1.5, 8, 1, 9]
+y = [2, 8, 1.8, 8, 0.6, 11]
+
+plt.scatter(x,y)
+plt.show()
+
+x = np.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
